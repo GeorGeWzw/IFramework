92d86b635a7889b2b9d9c191db3545af57d53f35
IvanZheng <yvonne_7_7@hotmail.com>
2014-09-18 10:16:38 +0800

    ignore json serialize

--- Src/iFramework/Domain/Entity.cs
+++ Src/iFramework/Domain/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ namespace IFramework.Domain
 {
     public class Entity : IEntity
     {
+        [JsonIgnore]
         public object DomainContext { get; set; }
     }
 }
